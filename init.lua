94e6e57f7507f817fae9c82cdc0bd3bf96539484
yijing1998 <yijing1998@126.com>
2017-03-12 19:17:15 +0800

    init.lua框架

---
+++ init.lua
@@ -0,0 +1,34 @@
+if (wifi.getmode() ~= wifi.STATION) then
+    wifi.setmode(wifi.STATION, true)
+end
+
+local function smartconfig()
+    local t_smart = tmr.create()
+    local tout_smart = 30000
+    wifi.stopsmart()
+    wifi.startsmart()
+    wifi.sta.eventMonReg(wifi.STA_GOTIP, function()
+        t_smart:unregister()
+        wifi.sta.eventMonReg(wifi.STA_GOTIP)
+        print("successful get ip address, stop the timer and begin user application!")
+        --do user application
+    end)
+    wifi.sta.eventMonStart()
+    t_smart:alarm(tout_smart, tmr.ALARM_SINGLE, function()
+        print("smartconfig timeout, redo smartconfig")
+        wifi.sta.eventMonReg(wifi.STA_GOTIP)
+        smartconfig()
+    end)
+end
+
+local t_powerup = tmr.create()
+local tout_powerup = 15000
+t_powerup:alarm(tout_powerup, tmr.ALARM_SINGLE, function()
+    if wifi.sta.getip() == nil then
+        print("power up time out, no successful wifi connect, begin smartconfig!")
+        smartconfig()
+    else
+        print("power up successful, wifi connceted! begin user application!")
+        --do user application
+    end
+end)
